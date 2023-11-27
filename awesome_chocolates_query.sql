4c8409891e59109de3db4ab23d633298a337ec93
Saimun Hassan <123021229+saimun4all@users.noreply.github.com>
2023-11-26 19:02:35 -0500

    Analyzed the data by running some SQL queries

--- awesome_chocolates_query.sql
+++ awesome_chocolates_query.sql
@@ -1,4 +1,9 @@
-SELECT * FROM
-`awesome chocolates`.`products`
+SELECT Geo FROM
+`awesome chocolates`.`sales` AS s
+INNER JOIN `awesome chocolates`.`geo` AS g
+ON s.GeoID = g.GeoID
+WHERE s.GeoID = 'G4'
+
+
 
 
