2e2ad81fe785692d167c44b863081da9ad1ae76a
Saimun Hassan <123021229+saimun4all@users.noreply.github.com>
2023-11-26 20:34:56 -0500

    Analyzed lowest and highest no. of chocolate boxes shipped

--- awesome_chocolates_query.sql
+++ awesome_chocolates_query.sql
@@ -1,15 +1,20 @@
 -- *** Test Data with Particular conditions ***
 SELECT * FROM 
 `awesome chocolates`.`sales`
-WHERE GeoID = 'G4' AND Amount > 5000
+WHERE GeoID = 'G4' AND Amount > 5000;
 
 -- *** Find the region with GeoID G4 using JOIN ***
 SELECT s.* FROM
 	`awesome chocolates`.`sales` AS s
 	INNER JOIN `awesome chocolates`.`geo` AS g
 	ON s.GeoID = g.GeoID
-WHERE g.geo = 'Canada'
+WHERE g.geo = 'Canada';
 
+-- *** Find lowest and highest number of chocolate boxes shipped ***
+SELECT * FROM 
+	`awesome chocolates`.`sales`
+ORDER BY boxes;
 
-
+SELECT MAX(boxes) FROM 
+	`awesome chocolates`.`sales`
 
